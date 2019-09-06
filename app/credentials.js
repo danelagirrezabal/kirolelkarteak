1f3c2b27a6364128efe1fdc9fb4dd7fb15114030
JoanaAS <joana.agirreabal@gmail.com>
2019-09-06 20:16:56 +0200

    igotzen fitxategia

---
+++ app/credentials.js
@@ -0,0 +1,38 @@
+module.exports = {
+cookieSecret: 'your cookie secret goes here',
+gmail: {
+	user: 'kirolelkarteak@gmail.com',
+	password: 'adminkirolelkarteak',
+},
+twitterzkeskubaloia: {
+	consumer_key: 'zIhdpQAYKfAozabYKtt3ssw4f',
+	consumer_secret: 'SO9oIH7X7D2OKKsX2yMqdhVupSjugkeWNkt2SRmVMdOWEWY3iU',
+	access_token: '2227335151-jvY83BAwPcAX4S3hZ4YvwHKkkUD2DPBqxYWwLzg',
+	access_token_secret: 'lKLbYzfVE34yGBr09aKLlCGGzOMTnD6ezZeqw0DeUoe23',
+//screen_name: '',
+	timeout_ms: 60*1000
+},
+//twittereskubaloiaZKE: {
+twitter: {
+    consumer_key: 'HGlJgf90C64u3Z9TXOlKOAoxe',
+    consumer_secret: 'YTkLulDJ1qjl4mZjFvJIsO3eunkx7ZU87MYm8fsFebJHZMEvGZ',
+    access_token: '203467804-k5rOlskADqUaNljL3gIR6mFE8GEcOrWc4PbF6ICQ',
+    access_token_secret: 'kXilp8jL7ztMhEsmt4yTKeaO0xQYKE9R7Afd0wZSMeM69',
+//screen_name: '',
+    timeout_ms: 60*1000
+},
+dbdevelop: {
+        host: 'localhost',
+        user: 'root',
+        password : 'joanaagi',                    //root
+        port : 3306, //port mysql                   //8889 
+        database: 'heroku_3a7c26fa617acae'      //kirolElkarteak
+},
+dbproduction: {
+        host: 'us-cdbr-iron-east-04.cleardb.net',
+        user: 'b65e4830d842c6',
+        password : '5771113112b92f3',                   //'ff86419e'
+      //  port : 3306, //port mysql
+        database:'heroku_3a7c26fa617acae'
+}
+};
